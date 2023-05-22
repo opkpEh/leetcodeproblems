a216bb9522c47c9344510e6888bed780c540c760
opkpEh <harshpandeykp20@gmail.com>
2023-05-22 21:38:24 +0530

    better version using hashsets

--- 217 Contains Duplicate.py
+++ 217 Contains Duplicate.py
@@ -1,25 +1,37 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        
-        nums.sort()
-        ind = 0
-        ind2= 1
-        to_return= False
-        
-        while ind < len(nums):
-            
-            if ind2==len(nums):
-                break
-            
-            if(nums[ind]== nums[ind2]):
-                to_return= True
-                    
-            ind= ind+1
-            ind2= ind2+1
-             
-        return to_return
-        
+#class Solution(object):
+#    def containsDuplicate(self, nums):
+#        
+#        nums.sort()
+#        ind = 0
+#        ind2= 1
+#        to_return= False
+#        
+#        while ind < len(nums):
+#            
+#            if ind2==len(nums):
+#                break
+#            
+#            if(nums[ind]== nums[ind2]):
+#                to_return= True
+#                    
+#            ind= ind+1
+#            ind2= ind2+1
+#             
+#        return to_return
+#        
+#
+#a = Solution()
+# 
+#print(a.containsDuplicate([1,2,3,4,4]))
 
-a = Solution()
-
-print(a.containsDuplicate([1,2,3,4,4]))
+class Solution:
+      def containsDuplicate(self, nums):
+            
+            hashset= set()
+            
+            for n in nums:
+                  if n in hashset:
+                        return True
+                  hashset.add(n)
+                  
+            return False
