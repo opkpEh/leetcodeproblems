d2c02bc2f553b9c88f555b5539f44f088bff040f
opkpEh <harshpandeykp20@gmail.com>
2023-05-22 21:22:00 +0530

    first problem done

---
+++ 217 Contains Duplicate.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        
+        nums.sort()
+        ind = 0
+        ind2= 1
+        to_return= False
+        
+        while ind < len(nums):
+            
+            if ind2==len(nums):
+                break
+            
+            if(nums[ind]== nums[ind2]):
+                to_return= True
+                    
+            ind= ind+1
+            ind2= ind2+1
+             
+        return to_return
+        
+
+a = Solution()
+
+print(a.containsDuplicate([1,2,3,4,4]))
