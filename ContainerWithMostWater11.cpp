dd8d1559f5ec7ecdf145f707e74a9cf11f8e289c
Kushagra Pandey <56930383+opkpEh@users.noreply.github.com>
2023-10-23 17:19:44 +0530

    Update ContainerWithMostWater11.cpp

--- ContainerWithMostWater11.cpp
+++ ContainerWithMostWater11.cpp
@@ -1,6 +1,5 @@
-#include <iostream>
 #include <vector>
-#include <cmath>
+#include <iostream>
 
 using namespace std;
 
@@ -9,19 +8,20 @@ class Solution
 public: 
 	int maxArea(std::vector<int> &height)
 	{
-	int p1, p2= height.size(), waternow , vol=0, b , width;
+	int p1=0, p2= height.size()-1;
+	int waternow=0, vol=0 , width=0;
 
 	while(p1<p2)
 	{
 		width= p2-p1;
 
-		b= min(height[p1], height[p2]);
+		int b= min(height[p1], height[p2]);
 
 		waternow = width * b;
 		
-		vol = max(waternow, vol);
+		vol = max(vol, waternow);
 
-		if(height[p1]>height[p2])
+		if(height[p1]<height[p2])
 			p1++;
 		else
 			p2--;
@@ -33,12 +33,10 @@ public:
 
 int main()
 {
-	Solution sol;
-	vector<int> test= {1,8,6,2,5,4,8,3,7}; 
-	
-	cout<<"working"<<endl;
-	cout<<sol.maxArea(test);
+      Solution s;
 
-	return 0;
+      std::vector<int> v= {1,8,6,2,5,4,8,3,7};
 
+      cout<<s.maxArea(v);
+      return 0;
 }
