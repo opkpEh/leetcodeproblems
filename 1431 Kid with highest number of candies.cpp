945526dc53ea52158f182807c23eb10d82f8ccd1
Kushagra Pandey <56930383+opkpEh@users.noreply.github.com>
2023-10-23 18:29:36 +0530

    Update 1431 Kid with highest number of candies.cpp

--- 1431 Kid with highest number of candies.cpp
+++ 1431 Kid with highest number of candies.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector <bool> ans = {};
 
+        vector<int> Scandies = candies;
+        sort(Scandies.begin(), Scandies.end());
+
+        for(auto candy: candies)
+        {
+            if(candy+extraCandies>= Scandies.back())
+            {
+                ans.push_back(true);
+            }
+
+            else
+            {
+                ans.push_back(false);
+            }
+        }
+
+        return ans;
+    }
+};
